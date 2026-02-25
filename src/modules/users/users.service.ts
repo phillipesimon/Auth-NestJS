48cbfd2eeaf14f77ec6ab681bd993d94b4406809
Simon <phillipe.simon@gmail.com>
2026-02-25 15:07:20 -0300

    Creating password hashes and user search.

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,20 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './user.schema';
 import { Model } from 'mongoose';
 import { UserCreateDTO } from './user.dto';
 import { randomUUID } from 'crypto';
+import { hash } from 'bcrypt';
 
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
   async create(userDTO: UserCreateDTO) {
+    const userExists = await this.userModel.findOne({
+      username: userDTO.username,
+    });
+
+    if (userExists) {
+      throw new HttpException('User already exists!', HttpStatus.BAD_REQUEST);
+    }
+
+    const password = await hash(userDTO.password, 10);
+
     const userCreated = new this.userModel({
       ...userDTO,
       id: randomUUID(),
+      password,
     });
-    const user = userCreated.save();
+
+    const user = await userCreated.save();
     console.log(user);
     return user;
   }
